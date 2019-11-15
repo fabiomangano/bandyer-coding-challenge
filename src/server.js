dff9fd000d7e0df4a6a1c735d2d7e301d7589cc7
fabiomangano <fabiomangano100@gmail.com>
2019-11-15 20:31:25 +0100

    Create basic server routes

---
+++ src/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.post('/istants', (req, res) => {
+
+});
+app.get('/istants/:id', (req, res) => {
+
+});
+app.get('/istants', (req, res) => {
+
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
