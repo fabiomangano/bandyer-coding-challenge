e225f10c778c801fb3a4ed08a4848aea319e0672
fabiomangano <fabiomangano100@gmail.com>
2019-11-21 22:29:29 +0100

    Add uploader, file extension filter, save after upload (controller)

--- src/controllers/routes/instants/controllers.js
+++ src/controllers/routes/instants/controllers.js
@@ -1,4 +1,20 @@
 const Instant = require('../models/instant');
+const multer = require('multer');
+
+const {
+  PUBLIC_ORIGINAL_PHOTO_FOLDER,
+  UPLOADS_FOLDER,
+} = require('../../config');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join('src', UPLOADS_FOLDER));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `photo-${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
 /*
 GET /instants route to retrieve all instants.
  */
@@ -14,8 +30,45 @@ function getInstants(req, res) {
  * POST /instances to save a new instant.
  */
 function postInstant(req, res) {
-  res.status(200).json({
-    message: 'instant created'
+  const upload = multer({ storage: storage, fileFilter: utils.imageFilter }).single('file');
+
+  upload(req, res, (err) => {
+    // req.file contains information of uploaded file
+    // req.body contains information of text fields, if there were any
+
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    } else if (!req.file) {
+      res.status(400);
+      return res.send({message: 'Please select an image to upload'});
+    } else if (err instanceof multer.MulterError) {
+      res.status(500);
+      return res.send(err);
+    } else if (err) {
+      res.status(500);
+      return res.send(err);
+    }
+  
+    const image = fs.readFileSync(path.join('src', UPLOADS_FOLDER, req.file.filename));
+
+    const newInstant = new Instant({
+      uploaded: path.join(SERVER_URL, PUBLIC_ORIGINAL_PHOTO_FOLDER, req.file.filename),
+      name: req.body.name,
+      createdBy: req.body.createdBy,
+      weight: req.file.size, 
+    });
+  
+    newInstant.save(async(err, savedInstant) => {
+      if (err) {
+        return res.send(err);
+      }
+      //@TODO: resizeImage task
+
+      res.status(200).json({
+        message: 'Instant successfully added!',
+        savedInstant
+      });
+    })  
   });
 }
 
