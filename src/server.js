8b314d8202f525f4ba7c2e08939ff4dfbbee9bed
fabiomangano <fabiomangano100@gmail.com>
2019-11-21 21:53:30 +0100

    Create dotenv file for dev environment and replace env variables on server

--- src/server.js
+++ src/server.js
@@ -1,21 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const instantsRoutes = require('./controllers/routes/instants');
-const PORT = 3000;
+
+const {
+  SERVER_PORT,
+  MONGODB_SERVER_URL,
+} = process.env;
 
 mongoose
-  .connect(
-    'mongodb://mongo:27017/expressmongo',
-    { useNewUrlParser: true }
-  )
+  .connect(MONGODB_SERVER_URL, {
+    useNewUrlParser: true,
+  })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
-  
+
 app.use('/', instantsRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}!`);
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running on port ${SERVER_PORT}!`);
 });
 
 module.exports = app;
