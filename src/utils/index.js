4ed2debeb62dad6a8507a094e81c1173480fb34b
fabiomangano <fabiomangano100@gmail.com>
2019-11-21 22:55:16 +0100

    Create resizeImage utility

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
+const Jimp = require('jimp');
+
+const {
+  RESIZED_PHOTO_FOLDER,
+  DEFAULT_RESIZED_WIDTH,
+  DEFAULT_RESIZED_HEIGHT,
+} = require('../config');
+
 const imageFilter = function(req, file, cb) {
   // Accept images only
   if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
@@ -7,6 +15,18 @@ const imageFilter = function(req, file, cb) {
   cb(null, true);
 };
 
+async function resizeImage({uploadedPath, filename}) {
+  try {
+    const image = await Jimp.read(uploadedPath);
+    image
+      .cover(DEFAULT_RESIZED_WIDTH, DEFAULT_RESIZED_HEIGHT)
+      .write(`src/${RESIZED_PHOTO_FOLDER}/${filename}`);
+  } catch (err) {
+    throw err;
+  }
+}
+
 module.exports = {
   imageFilter,
+  resizeImage,
 };
