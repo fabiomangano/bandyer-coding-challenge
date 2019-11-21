f474b8e1b90d9449a56b174f15ae10c9a965b0d9
fabiomangano <fabiomangano100@gmail.com>
2019-11-21 21:59:32 +0100

    Create config file with constant values

---
+++ src/config.js
@@ -0,0 +1,24 @@
+// Folders
+const UPLOADS_FOLDER = 'uploads';
+const RESIZED_PHOTO_FOLDER = 'instants';
+
+const PUBLIC_PHOTO_FOLDER = 'photos';
+const PUBLIC_ORIGINAL_PHOTO_FOLDER = 'photos-original';
+
+// Instant
+const INSTANT_STATUS = {
+  PENDING: 'Pending',
+  DONE: 'Done',
+};
+const DEFAULT_RESIZED_WIDTH = 140;
+const DEFAULT_RESIZED_HEIGHT = 140;
+
+module.exports = {
+  PUBLIC_PHOTO_FOLDER,
+  RESIZED_PHOTO_FOLDER,
+  UPLOADS_FOLDER,
+  INSTANT_STATUS,
+  PUBLIC_ORIGINAL_PHOTO_FOLDER,
+  DEFAULT_RESIZED_WIDTH,
+  DEFAULT_RESIZED_HEIGHT,
+};
